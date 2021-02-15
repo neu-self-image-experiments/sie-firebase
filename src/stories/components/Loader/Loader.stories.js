69540b8a367ac7f67c8555c0e1af027154028749
Alessia Pizzoccheri <pizzoccheri.alessia5@gmail.com>
2021-02-14 19:39:09 -0500

    [SIE-46] new Loader component

---
+++ src/stories/components/Loader/Loader.stories.js
@@ -0,0 +1,19 @@
+import '../../../scss/styles.scss';
+import React from 'react';
+import { Loader } from './Loader';
+
+/**
+ * Example Component: Loader
+ */
+export default {
+    title: 'Example/Loader',
+    component: Loader,
+};
+
+const Template = (args) => <Loader {...args} />;
+
+// Primary button
+export const Default = Template.bind({});
+Default.args = {
+    text: 'Primary Loader',
+};
