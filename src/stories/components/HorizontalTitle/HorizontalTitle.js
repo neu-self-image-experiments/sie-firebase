d34e7085f59c2e522afdc39130502ffff5e02bef
QA Hoang <qahoang317@gmail.com>
2021-02-15 17:29:12 -0800

    Added horizontal-title component

---
+++ src/stories/components/HorizontalTitle/HorizontalTitle.js
@@ -0,0 +1,45 @@
+import './styles.scss';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+/**
+ * HorizontalTitle Component
+ *
+ * @component
+ * @param {eyebrow} eyebrow
+ * @param {title} title
+ * @param {content} paragraph content
+ * @return {object}
+ */
+export const HorizontalTitle = (
+    { modifierClasses, eyebrow, title, content }) => {
+    return (
+        <div
+            className={
+                ['horizontal-title', `${modifierClasses}`].join(' ').trim()
+            }
+        >
+            {eyebrow ?
+                <div className="horizontal-title__eyebrow">{eyebrow}</div> : ''
+            }
+            <h1 className="horizontal-title__title">{title}</h1>
+            <p className="horizontal-title__content">{content}</p>
+        </div>
+    );
+};
+
+HorizontalTitle.propTypes = {
+    modifierClasses: PropTypes.string,
+    eyebrow: PropTypes.string,
+    title: PropTypes.string,
+    content: PropTypes.string,
+};
+
+HorizontalTitle.defaultProps = {
+    modifierClasses: '',
+    eyebrow: '',
+    title: '',
+    content: '',
+};
---
+++ src/stories/components/HorizontalTitle/HorizontalTitle.stories.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import '../../../scss/styles.scss';
+import { HorizontalTitle } from './HorizontalTitle';
+
+/**
+ * Example Component: HorizontalTitle
+ */
+export default {
+    title: 'Example/HorizontalTitle',
+    component: HorizontalTitle,
+};
+
+const Template = (args) => {
+    return (
+        <HorizontalTitle {...args} />
+    );
+};
+
+// Default HorizontalTitle
+export const Default = Template.bind({});
+Default.args = {
+    modifierClasses: '',
+    eyebrow: 'Eyebrow',
+    title: 'This is a default paragraph',
+    content:
+        <p>Lorem ipsum dolor sit amet, an has summo iriure epicuri,
+        has illud rationibus et. Prima ridens sit te, nam idque explicari
+        expetendis in. An mei adolescens mnesarchum, ei periculis adipiscing
+        per, probo populo nec ad. At vel consetetur moderatius, duo possim
+        iriure iracundia ea. Eam mazim efficiantur delicatissimi ei.
+        Duo at ornatus omnesque conclusionemque, ullum periculis in mei,
+        quo no sanctus eloquentiam</p>,
+};
+
+// Medium paragraph
+export const Medium = Template.bind({});
+Medium.args = {
+    modifierClasses: 'horizontal-title--medium',
+    eyebrow: 'Eyebrow',
+    title: 'This is a medium paragraph',
+    content:
+        <p>Lorem ipsum dolor sit amet, an has summo iriure epicuri,
+        has illud rationibus et. Prima ridens sit te, nam idque explicari
+        expetendis in. An mei adolescens mnesarchum, ei periculis adipiscing
+        per, probo populo nec ad. At vel consetetur moderatius, duo possim
+        iriure iracundia ea. Eam mazim efficiantur delicatissimi ei.
+        Duo at ornatus omnesque conclusionemque, ullum periculis in mei,
+        quo no sanctus eloquentiam.</p>,
+};
+
+// Large paragraph
+export const Large = Template.bind({});
+Large.args = {
+    modifierClasses: 'horizontal-title--large',
+    eyebrow: 'Eyebrow',
+    title: 'This is a large paragraph',
+    content:
+        <p>Lorem ipsum dolor sit amet, an has summo iriure epicuri,
+        has illud rationibus et. Prima ridens sit te, nam idque explicari
+        expetendis in. An mei adolescens mnesarchum, ei periculis adipiscing
+        per, probo populo nec ad. At vel consetetur moderatius, duo possim
+        iriure iracundia ea. Eam mazim efficiantur delicatissimi ei.
+        Duo at ornatus omnesque conclusionemque, ullum periculis in mei,
+        quo no sanctus eloquentiam.</p>,
+};
