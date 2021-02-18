6ce53c02526aab2fa3d99b2077b44a7e10a40ae8
Jason Ven <venjason8@gmail.com>
2021-02-18 12:44:45 -0800

    Adding Quaternary button and small button variations

--- src/stories/components/Button/Button.stories.js
+++ src/stories/components/Button/Button.stories.js
@@ -19,10 +19,26 @@ Primary.args = {
     text: 'Primary Button',
 };
 
+// Primary button small
+export const PrimarySmall = Template.bind({});
+PrimarySmall.args = {
+    modifierClasses: 'button--small',
+    url: '#',
+    text: 'Primary Button',
+};
+
 // Secondary button
 export const Secondary = Template.bind({});
 Secondary.args = {
     modifierClasses: 'button--secondary',
+    url: '#',
+    text: 'Secondary Button',
+};
+
+// Secondary button small
+export const SecondarySmall = Template.bind({});
+SecondarySmall.args = {
+    modifierClasses: 'button--secondary button--small',
     url: '#',
     text: 'Secondary Button',
 };
@@ -33,4 +49,28 @@ Tertiary.args = {
     modifierClasses: 'button--tertiary',
     url: '#',
     text: 'Tertiary Button',
+};
+
+// Tertiary button small
+export const TertiarySmall = Template.bind({});
+TertiarySmall.args = {
+    modifierClasses: 'button--tertiary button--small',
+    url: '#',
+    text: 'Tertiary Button',
+};
+
+// Quaternary button
+export const Quaternary = Template.bind({});
+Quaternary.args = {
+    modifierClasses: 'button--quaternary',
+    url: '#',
+    text: 'Quaternary Button',
+};
+
+// Quaternary button small
+export const QuaternarySmall = Template.bind({});
+QuaternarySmall.args = {
+    modifierClasses: 'button--quaternary button--small',
+    url: '#',
+    text: 'Quaternary Button',
 };
