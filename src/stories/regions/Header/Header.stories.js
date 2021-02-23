6c40973df91c4b26d34243cf2a029823bc82d590
Alessia Pizzoccheri <pizzoccheri.alessia5@gmail.com>
2021-02-23 01:07:54 -0500

    [SIE-16] create Header component

---
+++ src/stories/regions/Header/Header.stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../../../scss/styles.scss';
+import { Branding } from '../../components/Branding/Branding';
+import { Header } from './Header';
+
+/**
+ * Example Component: Sidebar
+ */
+export default {
+    title: 'Example/Header',
+    component: Header,
+};
+
+const Template = (args) => {
+    return (
+        <Header {...args} />
+    );
+};
+
+// Default Header
+export const Default = Template.bind({});
+Default.args = {
+    modifierClasses: '',
+    leftContent: <Branding text="SIE" />,
+    rightContent:
+        <a href="#">
+            Some Link
+        </a>,
+};
