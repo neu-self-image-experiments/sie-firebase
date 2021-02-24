3a05673237727059f70aa04af71dbf4c550a7654
bleachalon <bleachalon@qq.com>
2021-02-23 20:51:24 -0800

    fixed config variable and .env file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,17 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-// Due to env not working will put here for now cuz its blocking.
-const config = {
-    REACT_APP_FIREBASE_API_KEY: 'AIzaSyAXaRZRwMxLoe-b1BxqBnJjbdd7NZF-FEY',
-    REACT_APP_FIREBASE_AUTH_DOMAIN: 'cs6510-spr2021.firebaseapp.com',
-    REACT_APP_PROJECT_ID: 'cs6510-spr2021',
-    REACT_APP_STORAGE_BUCKET: 'cs6510-spr2021.appspot.com',
-    REACT_APP_MSG_SENDER_ID: '796249853280',
-    REACT_APP_APP_ID: '1:796249853280:web:56dc4cd01ba8bd8f7be205',
-    REACT_APP_MEASUREMENT_ID: 'G-NTJ3K8ST70',
-};
-
+// the following config does not work please refer to doc and make change locally.
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: config.REACT_APP_FIREBASE_API_KEY,
