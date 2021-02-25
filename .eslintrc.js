db795129a1696eab1957f43000b3182a2881bd0f
QA Hoang <qahoang317@gmail.com>
2021-02-24 23:27:51 -0800

    Added more rules and changed indentation to 2 spaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,42 @@
 module.exports = {
-    extends: ['google', 'plugin:react/recommended'],
-    parser: 'babel-eslint',
-    parserOptions: {
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 2020,
+  extends: ['google', 'plugin:react/recommended'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    plugins: ['react'],
-    rules: {
-        'indent': ['error', 4],
-        // suppress errors for missing 'import React' in files
-        'react/react-in-jsx-scope': 'off',
-        // allow jsx syntax in js files (for next.js project)
-        'react/jsx-filename-extension': [
-            1,
-            { 'extensions': ['.js', '.jsx'] },
-        ], // should add ".ts" if typescript project
-        'react/jsx-filename-extension': 'off',
-        'object-curly-spacing': [1, 'always'],
-        'require-jsdoc': 'off',
+    ecmaVersion: 2020,
+  },
+  plugins: ['react'],
+  rules: {
+    'indent': ['error', 2],
+    // suppress errors for missing 'import React' in files
+    'react/react-in-jsx-scope': 'off',
+    // allow jsx syntax in js files (for next.js project)
+    'react/jsx-filename-extension': [
+      1,
+      { 'extensions': ['.js', '.jsx'] },
+    ], // should add ".ts" if typescript project
+    'react/jsx-filename-extension': 'off',
+    'object-curly-spacing': [1, 'always'],
+    'require-jsdoc': 'off',
+    'no-console': 'error',
+    'no-empty-function': 'error',
+    'no-floating-decimal': 'error',
+    'no-irregular-whitespace': 'off',
+    'no-param-reassign': 'error',
+    'no-template-curly-in-string': 'error',
+    'no-unused-expressions': 'error',
+    'no-unused-vars': ['error', { args: 'none' }],
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'one-var': ['error', 'never'],
+    'prefer-const': 'error',
+  },
+  settings: {
+    react: {
+      version: 'latest',
     },
-    settings: {
-        react: {
-            version: 'latest',
-        },
-    },
+  },
 };
