b86ba60f0dfc30a1c321655b23e20de5654d0c39
Alessia Pizzoccheri <pizzoccheri.alessia5@gmail.com>
2021-02-28 01:21:07 -0500

    [SIE-35] reset file userService file name

--- src/stories/pages/Login/Login.js
+++ src/stories/pages/Login/Login.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import UserServices from '../../../firebase/CRUDServices/userServices1';
+import UserServices from '../../../firebase/CRUDServices/userServices';
 import { SplitGradient } from '../../layouts/SplitGradient/SplitGradient';
 import { Header } from '../../layouts/Header/Header';
 import { Footer } from '../../layouts/Footer/Footer';
