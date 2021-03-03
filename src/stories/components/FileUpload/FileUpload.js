876976d5131030f0f099055419bb1dbf59670aa1
Daniel English <denglish7@gmail.com>
2021-03-03 11:53:08 -0800

    made requested changes

--- src/stories/components/FileUpload/FileUpload.js
+++ src/stories/components/FileUpload/FileUpload.js
@@ -21,12 +21,27 @@ export const FileUpload = ({ onClick }) => {
     </div>
   );
 
+  const showFileName = () => {
+    if (document.getElementById('file-upload__input')) {
+      const name = document.getElementById('file-upload__input');
+      const selectedFile = name.files.item(0).name;
+      document.getElementById('file-upload__selected').innerHTML = selectedFile;
+    }
+  };
+
   return (
     <div className="file-upload-container">
       <h3>Upload a photo instead</h3>
       <div className="file-upload">
-        <input type="file" />
-        <Button text={buttonText} onClick={onClick} />
+        <label htmlFor="file-upload__input" className="file-upload__label">
+            Browse...
+        </label>
+        <p id="file-upload__selected">No file selected.</p>
+        <input id="file-upload__input" type="file" onChange={showFileName} />
+        <Button
+          modifierClasses='button--small'
+          text={buttonText}
+          onClick={onClick} />
       </div>
     </div>
   );
