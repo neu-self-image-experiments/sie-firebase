7dddf6aca18d029962381315d79f669de700d260
theodore3131 <alantheodore32@gmail.com>
2021-03-03 14:34:24 -0800

    listen to pub/sub using firebase functions

--- src/firebase/CRUDServices/imageService.js
+++ src/firebase/CRUDServices/imageService.js
@@ -1,13 +1,12 @@
 import firebase from '../firebase';
-const { PubSub } = require('@google-cloud/pubsub');
 
 export default class ImageService {
   constructor() {
     self.storageRef = firebase.storage().ref();
     self.sieRawImagesBucketRef = storageRef.child('sie-raw-images');
-    self.sieProcessedImagesBucketRef = storageRef.child('sie-processed-images');
-    self.subscriptionName =
-  'projects/cs6510-spr2021/subscriptions/sie-image-processing-result-test-sub';
+    self.sieProcessedImagesBucketRef = storageRef.child('sie-stimuli-images');
+    self.topicName =
+    'projects/cs6510-spr2021/topics/sie-image-processing-result-test';
   }
 
     // singleton instance.
@@ -18,7 +17,7 @@ export default class ImageService {
      * @return {ImageService} instance
      */
     static getInstance = () => {
-      if (self.sieInstance == null) {
+      if (!self.sieInstance) {
         self.sieInstance = new ImageService();
       }
       return self.sieInstance;
@@ -26,13 +25,13 @@ export default class ImageService {
 
     /**
      * Put a self image into the cloud storage.
-     * @param {String} userName user name.
+     * @param {String} userId user id.
      * @param {String} experimentId experiment id.
      * @param {File} image self image file.
      * @return {Boolean} succeed or not.
      */
-    postImage = async (userName, experimentId, image) => {
-      const folderPath = userName + '-' + experimentId + '/';
+    postImage = async (userId, experimentId, image) => {
+      const folderPath = userId + '-' + experimentId + '/';
       const imagePath = folderPath + image.name;
       const rawImageRef = self.sieRawImagesBucketRef.child(imagePath);
       rawImageRef.put(image).then(() => {
@@ -44,68 +43,63 @@ export default class ImageService {
 
     /**
      * Get all existing self images in the messaging queue.
-     * @param {String} userName user name.
+     * @param {String} userId user id.
      * @param {String} experimentId experiment id.
-     * @return {Image[]} array of self image files.
+     * @return {File[]} array of self image files.
      */
-    getImages = async (userName, experimentId) => {
-      // Creates a client; cache this for further use
-      pubSubClient = new PubSub();
-      // References an existing subscription
-      const subscription = pubSubClient.subscription(self.subscriptionName);
+    getImages = async (userId, experimentId) => {
+      const resultImages = [];
 
-      // Create an event handler to handle messages
-      const messageArray = [];
-      const messageHandler = (message) => {
-        messageArray.push(message.data);
-        // "Ack" (acknowledge receipt of) the message
-        message.ack();
-      };
+      firebase.functions.pubsub.topic(self.topicName).onPublish((message) => {
+        // Decode the PubSub Message body.
+        const messageBody = message.data ?
+          Buffer.from(message.data, 'base64').toString() : null;
 
-      // Listen for new messages until timeout is hit
-      subscription.on('message', messageHandler);
+        if (!messageBody) {
+          return [];
+        }
 
-      setTimeout(() => {
-        subscription.removeListener('message', messageHandler);
-      }, timeout * 1000);
-
-      // loop through the messages to locate those processed images
-      // message format: [userName]-[experimentId]-[status]
-      for (let index = 0; index < messageArray.length; index++) {
-        const message = messageArray[index];
-        const [msgUserName, msgExperimentId, status] = message.split('-');
-
-        if (msgUserName == userName && msgExperimentId == experimentId) {
-          if (status == 'complete') {
-            const folderPath = userName + '-' + experimentId + '/';
+        const [msgUserId, msgExperimentId, status] = messageBody.split('-');
+        if (msgUserId === userId && msgExperimentId === experimentId) {
+          if (status === 'complete') {
+            // get all processed images from cloud storage
+            const folderPath = userId + '-' + experimentId + '/';
             const processedImagesRef =
             self.sieProcessedImagesBucketRef.child(folderPath);
-            // TODO: download directly in the browser for all processed images
-            processedImagesRef.getDownloadURL().then((url) => {
-              const xhr = new XMLHttpRequest();
-              xhr.responseType = 'blob';
-              // xhr.onload = (event) => {
-              //   const blob = xhr.response;
-              // };
-              xhr.open('GET', url);
-              xhr.send();
+
+            processedImagesRef.getFiles((err, images) => {
+              if (!err) {
+                // files is an array of File objects.
+                images.forEach((image) => {
+                  image.getDownloadURL().then((url) => {
+                    const xhr = new XMLHttpRequest();
+                    xhr.responseType = 'blob';
+                    xhr.onload = (event) => {
+                      const blob = xhr.response;
+                      // xhr.response will be a Blob ready to save
+                      resultImages.push(blob);
+                    };
+                    xhr.open('GET', url);
+                    xhr.send();
+                  });
+                });
+              }
             });
-            // exist the loop if found the message
-            break;
           }
         }
-      }
+      });
+
+      return resultImages;
     }
 
-
     /**
-     * Delete self image by userName-experimentId in cloud storage.
-     * @param {String} userName user name.
+     * Delete self image by userId-experimentId in cloud storage.
+     * @param {String} userId user id.
      * @param {String} experimentId experiment id.
      * @return {Boolean} succeed or not.
      */
-    deleteImage = async (userName, experimentId) => {
-      const folderPath = userName + '-' + experimentId + '/';
+    deleteImage = async (userId, experimentId) => {
+      const folderPath = userId + '-' + experimentId + '/';
       const imagePath = folderPath + image.name;
       const rawImageRef = self.sieRawImagesBucketRef.child(imagePath);
       rawImageRef.delete().then(() => {
