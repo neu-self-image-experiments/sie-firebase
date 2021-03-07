bc5c28ad7ba2c440320f2e7af44c73b67100097c
Alessia Pizzoccheri <pizzoccheri.alessia5@gmail.com>
2021-03-07 16:54:08 -0500

    [SIE-84] small style changes

--- src/stories/pages/Experiment/Experiment.js
+++ src/stories/pages/Experiment/Experiment.js
@@ -34,118 +34,116 @@ export const Experiment = ({ title }) => {
 
   return (
     <Main>
-      <div>
-        <Header
-          leftContent={title}
-        />
-        <div className="experiment">
-          <Constrain modifierClasses="constrain--wide">
-            <Wizard labels={steps}>
-              <div className="step-1">
-                <h3>Introduction</h3>
-                <p>{title}</p>
-                <p>
-                  Lorem ipsum dolor sit amet, an has summo
-                  riure epicuri, has illud rationibus et. Prima ridens sit te,
-                  nam idque explicari expetendis in. An mei adolescens
-                  mnesarchum, ei periculis adipiscing per, probo populo nec ad.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, an has summo
-                  riure epicuri, has illud rationibus et. Prima ridens sit te,
-                  nam idque explicari expetendis in. An mei adolescens
-                  mnesarchum, ei periculis adipiscing per, probo populo nec ad.
-                </p>
-              </div>
-              <div className="step-2">
-                <h3>Consent Form</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, an has summo
-                  riure epicuri, has illud rationibus et. Prima ridens sit te,
-                  nam idque explicari expetendis in. An mei adolescens
-                  mnesarchum, ei periculis adipiscing per, probo populo nec ad.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, an has summo
-                  riure epicuri, has illud rationibus et. Prima ridens sit te,
-                  nam idque explicari expetendis in. An mei adolescens
-                  mnesarchum, ei periculis adipiscing per, probo populo nec ad.
-                </p>
-              </div>
-              <div className="step-3">
-                <h3>Photo Instructions and Upload</h3>
-                <PhotoInstructions />
-                <WebcamControls />
-              </div>
-              <div className="step-4">
-                <h3>Pre-Survey</h3>
-                <p>Please, answer the questionnaire below.</p>
-                <p>
-                  Lorem ipsum dolor sit amet, an has summo
-                  riure epicuri, has illud rationibus et. Prima ridens sit te,
-                  nam idque explicari expetendis in. An mei adolescens
-                  mnesarchum, ei periculis adipiscing per, probo populo nec ad.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, an has summo
-                  riure epicuri, has illud rationibus et. Prima ridens sit te,
-                  nam idque explicari expetendis in. An mei adolescens
-                  mnesarchum, ei periculis adipiscing per, probo populo nec ad.
-                </p>
-              </div>
-              <div className="step-5">
-                <h3>Image Selection Task</h3>
-                <p>
-                  In the following computer task, you will complete a series of
-                  trials where you will be asked to make a split-second choice
-                  between two images of yourself.
-                </p>
-                <p>
-                  Each image is based on an actual photograph of you, but there
-                  is noise/static on top of the photo that may alter its
-                  appearance. The differences between the two images may be
-                  quite subtle, so we ask that you go with your gut.
-                </p>
-                <p>
-                  Put your middle or index fingers on the E and I keys of your
-                  keyboard. Two pictures will appear side by side on the screen.
-                  For each pair of pictures, we would like you to select the
-                  picture that is most like you. To select the image on the left
-                  side press the E key; to select the image on the right press
-                  the I key.
-                </p>
-                <p>
-                  This task is timed and will take approximately 20 minutes to
-                  complete. There are no wrong answers, so GO AS FAST AS YOU
-                  CAN. Responses that are too slow will not be recorded.
-                </p>
-                <p>
-                  To get started, go this [URL]
-                </p>
-              </div>
-              <div className="step-6">
-                <h3>Post-Survey</h3>
-                <p>Please, answer the questionnaire below.</p>
-                <p>
-                  Lorem ipsum dolor sit amet, an has summo
-                  riure epicuri, has illud rationibus et. Prima ridens sit te,
-                  nam idque explicari expetendis in. An mei adolescens
-                  mnesarchum, ei periculis adipiscing per, probo populo nec ad.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, an has summo
-                  riure epicuri, has illud rationibus et. Prima ridens sit te,
-                  nam idque explicari expetendis in. An mei adolescens
-                  mnesarchum, ei periculis adipiscing per, probo populo nec ad.
-                </p>
-              </div>
-              <div className="step-7">
-                <h3>Debriefing</h3>
-                <p>Thank you for participating in the experiment.</p>
-              </div>
-            </Wizard>
-          </Constrain>
-        </div>
+      <Header
+        leftContent={<h5>{title}</h5>}
+      />
+      <div className="experiment">
+        <Constrain modifierClasses="constrain--wide">
+          <Wizard labels={steps}>
+            <div className="step-1">
+              <h3>Introduction</h3>
+              <h4>{title}</h4>
+              <p>
+                Lorem ipsum dolor sit amet, an has summo
+                riure epicuri, has illud rationibus et. Prima ridens sit te,
+                nam idque explicari expetendis in. An mei adolescens
+                mnesarchum, ei periculis adipiscing per, probo populo nec ad.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, an has summo
+                riure epicuri, has illud rationibus et. Prima ridens sit te,
+                nam idque explicari expetendis in. An mei adolescens
+                mnesarchum, ei periculis adipiscing per, probo populo nec ad.
+              </p>
+            </div>
+            <div className="step-2">
+              <h3>Consent Form</h3>
+              <p>
+                Lorem ipsum dolor sit amet, an has summo
+                riure epicuri, has illud rationibus et. Prima ridens sit te,
+                nam idque explicari expetendis in. An mei adolescens
+                mnesarchum, ei periculis adipiscing per, probo populo nec ad.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, an has summo
+                riure epicuri, has illud rationibus et. Prima ridens sit te,
+                nam idque explicari expetendis in. An mei adolescens
+                mnesarchum, ei periculis adipiscing per, probo populo nec ad.
+              </p>
+            </div>
+            <div className="step-3">
+              <h3>Photo Instructions and Upload</h3>
+              <PhotoInstructions />
+              <WebcamControls />
+            </div>
+            <div className="step-4">
+              <h3>Pre-Survey</h3>
+              <p>Please, answer the questionnaire below.</p>
+              <p>
+                Lorem ipsum dolor sit amet, an has summo
+                riure epicuri, has illud rationibus et. Prima ridens sit te,
+                nam idque explicari expetendis in. An mei adolescens
+                mnesarchum, ei periculis adipiscing per, probo populo nec ad.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, an has summo
+                riure epicuri, has illud rationibus et. Prima ridens sit te,
+                nam idque explicari expetendis in. An mei adolescens
+                mnesarchum, ei periculis adipiscing per, probo populo nec ad.
+              </p>
+            </div>
+            <div className="step-5">
+              <h3>Image Selection Task</h3>
+              <p>
+                In the following computer task, you will complete a series of
+                trials where you will be asked to make a split-second choice
+                between two images of yourself.
+              </p>
+              <p>
+                Each image is based on an actual photograph of you, but there
+                is noise/static on top of the photo that may alter its
+                appearance. The differences between the two images may be
+                quite subtle, so we ask that you go with your gut.
+              </p>
+              <p>
+                Put your middle or index fingers on the E and I keys of your
+                keyboard. Two pictures will appear side by side on the screen.
+                For each pair of pictures, we would like you to select the
+                picture that is most like you. To select the image on the left
+                side press the E key; to select the image on the right press
+                the I key.
+              </p>
+              <p>
+                This task is timed and will take approximately 20 minutes to
+                complete. There are no wrong answers, so GO AS FAST AS YOU
+                CAN. Responses that are too slow will not be recorded.
+              </p>
+              <p>
+                To get started, go this [URL]
+              </p>
+            </div>
+            <div className="step-6">
+              <h3>Post-Survey</h3>
+              <p>Please, answer the questionnaire below.</p>
+              <p>
+                Lorem ipsum dolor sit amet, an has summo
+                riure epicuri, has illud rationibus et. Prima ridens sit te,
+                nam idque explicari expetendis in. An mei adolescens
+                mnesarchum, ei periculis adipiscing per, probo populo nec ad.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, an has summo
+                riure epicuri, has illud rationibus et. Prima ridens sit te,
+                nam idque explicari expetendis in. An mei adolescens
+                mnesarchum, ei periculis adipiscing per, probo populo nec ad.
+              </p>
+            </div>
+            <div className="step-7">
+              <h3>Debriefing</h3>
+              <p>Thank you for participating in the experiment.</p>
+            </div>
+          </Wizard>
+        </Constrain>
       </div>
     </Main>
   );
