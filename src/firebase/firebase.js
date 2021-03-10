db45cec7273e9d885c7a98d38f67147f3318993c
QA Hoang <qahoang317@gmail.com>
2021-03-09 17:08:57 -0800

    Added storage import to firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/storage';
 import 'firebase/functions';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
