bbab96450ad947383e7fd8428f32e61829f14e84
QA Hoang <qahoang317@gmail.com>
2021-03-09 18:33:57 -0800

    Added constants.js files for future refactoring

---
+++ src/firebase/constants.js
@@ -0,0 +1,13 @@
+export const firestoreCollections = {
+  EXPERIMENT: 'experiments',
+  USER: 'users',
+};
+
+export const storageBuckets = {
+  SIE_RAW_IMGS: 'sie-raw-images',
+  SIE_STIMULI_IMGS: 'sie-stimuli-images',
+};
+
+export const pubsub = {
+  SIE_RESULT: 'projects/cs6510-spr2021/topics/sie-image-processing-result-test',
+};
