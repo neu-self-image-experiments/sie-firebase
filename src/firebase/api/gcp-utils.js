3342b3bc6b5a57ef0c072addb5fce27947e39b1a
theodore3131 <alantheodore32@gmail.com>
2021-03-18 12:05:52 -0700

    define callback for getting images & fix upload

--- src/firebase/api/gcp-utils.js
+++ src/firebase/api/gcp-utils.js
@@ -1,32 +1,31 @@
-import { storage, pubsub as gcpPubsub } from '../firebase.js';
-import { pubsub } from '../constants.js';
+import { app, pubsub as gcpPubsub } from '../firebase.js';
+import { pubsub, storageBuckets } from '../constants.js';
 
 /**
  * Upload an image to the specified bucket
  * @param {string} userId userId
  * @param {string} experimentId experimentId
  * @param {Blob} image image file to be uploaded
- * @param {string} bucket destination bucket
  *
  * @return {JSON}
  */
 export const uploadImageToStorage = async (
-  userId, experimentId, image, bucket) => {
+  userId, experimentId, image) => {
   const imagePath = `${userId}-${experimentId}/${image.name}`;
 
-  const storageRef = storage.ref();
-  const rawImageBucketRef = storageRef.child(bucket);
+  const rawImageBucketRef = app.storage(storageBuckets.SIE_RAW_IMGS).ref();
   const newImageRef = rawImageBucketRef.child(imagePath);
-  newImageRef.put(image).then((snapshot) => {
+  return await newImageRef.put(image).then(() => {
     return {
-      status: '201',
-      error: null,
+      status: 201,
+      message: 'image successfully uploaded',
+      data: {},
     };
-  }).catch((error) => {
+  }).catch((err) => {
     return {
-      status: '500',
-      data: null,
-      error,
+      status: 401,
+      message: `not authenticated ${err}`,
+      data: {},
     };
   });
 };
@@ -34,41 +33,50 @@ export const uploadImageToStorage = async (
 
 /**
  * Subscribes to pubsub to signal stimuli generation completion
+ * @param {Function} imageUrlsHandler react hook function for imageUrls
+ * @param {Function} errorHandler react hook function for error.
  */
-export const observeStimuliCompletion = async () => {
-  gcpPubsub.topic(pubsub.SIE_RESULT).onPublish((msg) => {
-    // Decode the PubSub Message body.
-    const msgBody = msg.data ?
-      Buffer.from(message.data, 'base64').toString() : null;
-    // eslint-disable-next-line no-unused-vars
-    const participantId = msg.attributes.participant_id;
-    // eslint-disable-next-line no-unused-vars
-    const experimentId = msg.attributes.experiment_id;
+export const observeStimuliCompletion =
+  async (imageUrlsHandler, errorHandler) => {
+    gcpPubsub.topic(pubsub.SIE_RESULT).onPublish((msg) => {
+      // Decode the PubSub Message body.
+      const status = msg.data ?
+        Buffer.from(message.data, 'base64').toString() : null;
+      const userId = msg.attributes.participant_id;
+      const experimentId = msg.attributes.experiment_id;
 
-    if (msgBody === 'completed') {
-      // maybe this should be handled by a react hook?
-      // call getFileUrlsFromBucket once ready to display stimuli
-    } else {
-      // likewise: maybe should be handled by a setError hook
-    }
-  });
-};
+      if (status === 'Completed') {
+        // call getFileUrlsFromBucket once ready to display stimuli
+        getFileUrlsFromBucket(userId, experimentId).then(() => {
+          imageUrlsHandler(imageUrls);
+        });
+      } else {
+        // “face_missing”, “failed”
+        // call setError() for image processing status, display on frontend
+        errorHandler(status);
+      }
+    });
+  };
 
 /**
  * Get all file urls from bucket to display in img tags
- * @param {string} bucket storage bucket
- * @param {string} bucketPrefix prefix to destination storage directory
+ * @param {String} userId user id
+ * @param {String} experimentId experiment id
+ * @return {String[]} array of processed self image urls
  */
-export const getFileUrlsFromBucket = async (bucket, bucketPrefix) => {
+const getFileUrlsFromBucket = async (userId, experimentId) => {
+  const bucketPrefix = `${userId}-${experimentId}`;
   const fileUrls = [];
-  const bucketRef = storage.ref().child(bucket).child(bucketPrefix);
-  bucketRef.getFiles((err, files) => {
+  const bucketRef = app.storage(storageBuckets.SIE_STIMULI_IMGS).ref();
+  const stimuliImagesRef = bucketRef.child(bucketPrefix);
+
+  await stimuliImagesRef.getFiles((err, files) => {
     if (!err) {
       files.forEach((file) => {
         file.getDownloadURL().then((url) => {
           fileUrls.push(url);
-        }).catch((error) => {
-          // TODO(qhoang) error handling
+        }).catch(() => {
+          return [];
         });
       });
     }
