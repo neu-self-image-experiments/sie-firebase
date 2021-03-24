544c063738e64a60b2a184593b8e23326250b4aa
Daniel English <denglish7@gmail.com>
2021-03-23 19:30:47 -0700

    removed comment

--- src/stories/pages/Login/Login.js
+++ src/stories/pages/Login/Login.js
@@ -45,7 +45,6 @@ export const Login = ({ isDarkTheme }) => {
         setError(response.errorMessage);
       } else {
         setError('');
-        // window.alert(response.email);
       }
     });
   };
