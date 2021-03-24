82c0c49401e6b569bb3d179a2ff29467abbdd24d
Daniel English <denglish7@gmail.com>
2021-03-23 18:35:56 -0700

    finished routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable spaced-comment */
 import './App.scss';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Login } from './stories/pages/Login/Login';
 import { Signup } from './stories/pages/Signup/Signup';
 import { Dashboard } from './stories/pages/Dashboard/Dashboard';
-// eslint-disable-next-line no-unused-vars
 import { AuthContext } from './contexts/auth-provider';
 import { useContext } from 'react';
 
@@ -18,9 +15,6 @@ function App() {
       <Switch>
         <Route path="/">
           {user ? <Dashboard/> : <Login/>}
-        </Route>
-        <Route path="/dashboard">
-          <Dashboard />
         </Route>
         <Route path="/login">
           <Login />
