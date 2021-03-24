0fb1842eef8472d1790e8b29c6260b52e1bcc25c
Fernando Winfield Murillo <winfieldmurillo.f@husky.neu.edu>
2021-03-24 15:12:54 -0400

    Finish ExperimentsPage implementation and add TODO comments

--- src/stories/pages/ExperimentsPage/ExperimentsPage.js
+++ src/stories/pages/ExperimentsPage/ExperimentsPage.js
@@ -1,14 +1,21 @@
+/*
+  TODOs
+    #1: Replace old user context with new auth context.
+    #2: Revise experiment information fields displayed in cards of slider.
+    #3: Divide fetched experiments into 'ongoing' and 'inactive'.
+*/
+
 import './styles.scss';
 
 import React, { useEffect, useState } from 'react';
 import { Slider } from '../../components/Slider/Slider';
 import { Card } from '../../components/Card/Card';
 import { AddExperiment } from '../AddExperiment/AddExperiment';
-// eslint-disable-next-line no-unused-vars
-import { Button } from '../../components/Button/Button';
 // eslint-disable-next-line max-len
 import { HorizontalTitle } from '../../components/HorizontalTitle/HorizontalTitle';
 import { getAllExperiments } from '../../../firebase/api/experiments';
+// TODO #1.1(fernandowinfield): remove the import below.
+import UserServices from '../../../firebase/CRUDServices/userServices';
 
 const PLACEHOLDER_EXPERIMENTS = [
   { title: 'Title 1', shortDesc: 'Short description' },
@@ -19,7 +26,7 @@ const PLACEHOLDER_EXPERIMENTS = [
   { title: 'Title 6', shortDesc: 'Short description' },
 ];
 
-// This probably needs to be moved to the App level/higher level component
+// TODO #1.2(fernandowinfield): replace this context with the new auth context.
 export const UserContext = React.createContext();
 
 /**
@@ -31,22 +38,27 @@ export const UserContext = React.createContext();
  * )
  */
 export const ExperimentsPage = () => {
-  // eslint-disable-next-line no-unused-vars
   const [logInState, setLogInState] = useState({ isLoggedIn: true });
-  // eslint-disable-next-line no-unused-vars
   const [allExperiments, setAllExperiments] = useState(PLACEHOLDER_EXPERIMENTS);
   const [error, setError] = useState();
 
-  // useEffect(() => {
-  //   userService.getCurrentUser(setLogInState).catch((e) => {
-  //     setLogInState({ isLoggedIn: false });
-  //     setError(e);
-  //   });
-  // }, [logInState.isLoggedIn]);
+  // TODO #1.3(fernandowinfield): remove the service below.
+  const userService = UserServices.getInstance();
+
+  // TODO #1.4(fernandowinfield): adjust `useEffect()` implementation below to
+  // use new auth context.
+  useEffect(() => {
+    userService.getCurrentUser(setLogInState).catch((e) => {
+      setLogInState({ isLoggedIn: false });
+      setError(e);
+    });
+  }, [logInState.isLoggedIn]);
 
   useEffect(() => {
     getAllExperiments().then((response) => {
       if (response.status === 200) {
+        // TODO #3.1(fernandowinfield): Filter ongoing and inactive experiments
+        // and set state for both instead of setting state for all.
         setAllExperiments(response.data);
       } else {
         setError(response.error);
@@ -54,30 +66,46 @@ export const ExperimentsPage = () => {
     });
   }, []);
 
-  const ongoingExperiments = allExperiments ?
-    allExperiments.map(({ title = 'Title of Experiment',
-      shortDesc = 'Short description of experiment',
-      opened = 'mm-dd-yyyy',
-      admin = 'Admin name',
-      researchers = ['Researcher Name'] }, i) =>
-      <Card
-        modifierClasses='card--active'
-        title={title}
-        body= {shortDesc}
-        opened={opened}
-        admin={admin}
-        researchers={researchers.join(', ')}
-        key={i}
-      />,
+  // TODO #3.2(fernandowinfield): Map ongoingExperiments instead of all.
+  const ongoingExperimentsSlides = allExperiments ?
+    allExperiments.map(
+      (
+        // TODO #2.1(fernandowinfield): add/remove fields below as needed.
+        { title = 'Title of Experiment',
+          shortDesc = 'Short description of experiment',
+          opened = 'mm-dd-yyyy',
+          admin = 'Admin name',
+          researchers = ['Researcher Name'],
+        },
+        i,
+      ) =>
+        <Card
+          modifierClasses='card--active'
+          title={title}
+          body= {shortDesc}
+          opened={opened}
+          admin={admin}
+          researchers={researchers.join(', ')}
+          key={i}
+        />,
     ) :
-    <div/>;
+    null;
 
-  const inactiveExperiments = PLACEHOLDER_EXPERIMENTS.map(
-    ({ title = 'Title of Experiment',
-      shortDesc = 'Short description of experiment',
-      opened = 'mm-dd-yyyy',
-      admin = 'Admin name',
-      researchers = ['Researcher Name'] }, i) =>
+  // TODO #3.3(fernandowinfield): Map inactiveExperiments instead of all and
+  // add ternary operator like in the ongoingExperiments mapping (to handle
+  // the case when there are no inactive experiments).
+  const inactiveExperimentsSlides = PLACEHOLDER_EXPERIMENTS.map(
+    (
+      // TODO #2.2(fernandowinfield): here too, add/remove fields below as
+      // needed.
+      { title = 'Title of Experiment',
+        shortDesc = 'Short description of experiment',
+        opened = 'mm-dd-yyyy',
+        admin = 'Admin name',
+        researchers = ['Researcher Name'],
+      },
+      i,
+    ) =>
       <Card
         modifierClasses='card--inactive'
         title={title}
@@ -112,9 +140,16 @@ export const ExperimentsPage = () => {
             <AddExperiment buttonText={'+'}
               buttonModifierClasses={'button--circular-small'} />
           </div>
-          <Slider>
-            {ongoingExperiments}
-          </Slider>
+          {ongoingExperimentsSlides ?
+            <Slider>
+              {ongoingExperimentsSlides}
+            </Slider> :
+            <Card
+              modifierClasses='card--teaser'
+              body='No current ongoing experiments. Click the + above to
+              create one.'
+            />
+          }
         </div>
 
         {/* Inactive experiments */}
@@ -124,8 +159,10 @@ export const ExperimentsPage = () => {
               INACTIVE EXPERIMENTS
             </h6>
           </div>
+          {/* TODO #3.4(fernandowinfield): Add ternary operator and content to
+          display when there are no inactive experiments */}
           <Slider>
-            {inactiveExperiments}
+            {inactiveExperimentsSlides}
           </Slider>
         </div>
 
