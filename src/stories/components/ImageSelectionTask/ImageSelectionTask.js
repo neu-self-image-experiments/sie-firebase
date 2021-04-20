547da743d2c0a47aea63ff27aa8b4195cae2f749
Jason Ven <venjason8@gmail.com>
2021-04-20 16:31:34 -0700

    Updating Modal/Button class to contain an additional parameter to be used to disable react button class

--- src/stories/components/ImageSelectionTask/ImageSelectionTask.js
+++ src/stories/components/ImageSelectionTask/ImageSelectionTask.js
@@ -48,7 +48,8 @@ export const ImageSelectionTask = ({
         too slow will not be recorded.
       </p>
       <p>To get started</p>
-      <Modal buttonText="Start Experiment" canClose={selectionTaskCompleted}>
+      <Modal buttonText="Start Experiment"
+        selectionTaskComplete={selectionTaskCompleted}>
         <JsPsych
           selectionTaskCompletionHandler={selectionTaskCompletionHandler}
         />
